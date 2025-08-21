339d21cad6e5cc2842c85a289aa6e8f21eb26fc5
CZY774 <yogaliuze@gmail.com>
2025-08-21 18:24:44 +0700

    Refactors: Updates favicon.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/logo-3.svg';
 	import { onMount } from 'svelte';
 
 	let { children } = $props();
