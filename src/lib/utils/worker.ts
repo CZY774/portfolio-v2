60392a947a90a0e00a9660ff1aedd342e35149a6
CZY774 <yogaliuze@gmail.com>
2026-06-24 15:09:33 +0700

    fix: Remove unused worker.ts utility functions for cleanup

--- src/lib/utils/worker.ts
+++
@@ -1,9 +0,0 @@
-export const createWorker = (path: string) => {
-	if (typeof Worker === 'undefined') return null;
-	const worker = new Worker(path);
-	return {
-		post: (type: string, data: unknown) => worker.postMessage({ type, data }),
-		on: (callback: (e: MessageEvent) => void) => (worker.onmessage = callback),
-		terminate: () => worker.terminate()
-	};
-};
